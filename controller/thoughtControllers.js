96bda87cf9972317f5d7b3267527bd00d60a592c
Jondo <chet9880@gmail.com>
2022-08-19 00:51:00 -0400

    added user controllers

---
+++ controller/thoughtControllers.js
@@ -0,0 +1,5 @@
+// get all thoughts
+// get a single thought
+// post a thought
+// put update a thought by id
+// delete a thought by id
---
+++ controller/userController.js
@@ -0,0 +1,51 @@
+// get all users
+// get a single user by id
+// post a new user
+// put update a user by id
+// delete a user by id
+const { User} = require("../models")
+
+
+const usersControllers = {
+
+getAllUsers(req, res) {
+    User
+    .find({})
+    .then((data) => res.json(data))
+},
+
+getUserById(req, res) {
+    User
+    .findOne({_id: req.params.userId})
+    .populate({
+        path: "friends"
+    })
+    .populate({
+        path: "thoughts"
+    })
+    .then((data) => res.json(data))
+},
+
+createUser(req, res) {
+    User
+    .create(req.body)
+    .then((data) => res.json(data))
+},
+
+updateUser(req, res) {
+    User
+    .findOneAndUpdate({_id: req.params.userId}, req.body, {
+        new: true,
+        runValidators: true
+    })
+    .then((data) => res.json(data))
+},
+
+deleteUser(req, res){
+    User
+    .findOneAndDelete({_id: req.params.userId})
+    .then((data) => res.json(data))
+}
+}
+
+module.exports = usersControllers
